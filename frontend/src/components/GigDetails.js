71fbd4121da93cc854b4df5d3b1b5209c41ff66f
Abdullah Malik <95423490+Abdullah-Malik012@users.noreply.github.com>
2022-10-22 13:36:06 +0500

    done

--- frontend/src/components/GigDetails.js
+++ frontend/src/components/GigDetails.js
@@ -6,12 +6,13 @@ function GigDetails(props) {
       <li className='Gigdetails'>
         <a className='link' href={props.path}>
           <figure className='label' data-category={props.label}>
-            <img className='img' src={props.src}/>
+            <img className='img' src={props.src}/> 
           </figure>
           <div className='details'>
             <h5 className='text'>{props.text}</h5>
           </div> 
         </a>
+       
       </li>
     </>
   );
--- frontend/src/components/Gigs.js
+++ frontend/src/components/Gigs.js
@@ -1,46 +1,82 @@
 import React from 'react';
 import './Gigs.css';
 
+
+
+
 import GigDetails from './GigDetails';
 
 function Gigs() {
+
+  var data = [
+    { 
+        img : 'images/gig1.jpg', 
+        Name: "Mahad",
+        Details : "I can do c++ programming. OOP data structures"
+        
+        
+    },
+
+    {
+
+      img : 'images/gig2.png', 
+      Name : "hamza",
+      Details : "I can do webdevelopment. Using react express in html css js"
+    },
+
+    {
+
+      img : 'images/gig3.png', 
+      Name : "Ehsan",
+      Details : "I can do java programming. OOP DataStructures"
+
+    },
+
+    {
+
+      img : 'images/gig4.jpg', 
+      Name : "Abdullah",
+      Details : "I can do video editing. Software i use is adobe premier pro"
+
+    }
+
+  ]
   return (
+
+    
     <div className='Gigs'>
+     
      
       <div className='Gigscontainer'>
         <div className='Gigboundry'>
-          <ul className='Giginside '>
-            <GigDetails
-              src='images/gig1.jpg'
-              text='I can do C++ programming. OOP DataStructures'
-              label='Ali hamza'
-              path='https://www.fiverr.com/share/WDVjjL'
-            />
-            <GigDetails
-              src='images/gig2.png'
-              text='I can do webdevelopment. Using react express in html css js'
-              label='Mahad rahat'
-              path='https://www.fiverr.com/share/R9bvoR'
-            />
-         
           
+       
+          <ul className='Giginside '>
+          {data.map(({Name,Details, img})=>(
             <GigDetails
-              src='images/gig3.png'
-              text='I can do java programming. OOP DataStructures'
-              label='Ehsan Rasul'
-              path='https://www.fiverr.com/share/6NwPLP'
-            />
-            <GigDetails
-              src='images/gig4.jpg'
-              text='I can do video editing. Software i use is adobe premier pro'
-              label='Abdullah Malik'
-              path='https://www.fiverr.com/share/6NwPLP'
+            
+              src= {img}
+              text={Details}
+              label={Name}
+              path='https://www.fiverr.com/share/R9bvoR'
+
+             
             />
             
+
+            ))
+          }
+         
+           
+             
+         
           </ul>
+          
         </div>
       </div>
     </div>
+
+
   );
 }
 
